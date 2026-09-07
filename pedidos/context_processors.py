528238df49c9eb448855f31522a17f5c15dd9132
DanielBarri <daniel.barri@gmail.com>
2026-09-06 19:36:02 -0600

    Add rol_usuario context processor and update base template for vendor and admin links

--- pedidos/context_processors.py
+++ pedidos/context_processors.py
@@ -1,5 +1,10 @@
 from pedidos.carrito import Carrito
+from pedidos.views import es_vendedor
 
 
 def carrito_cantidad(request):
     return {'carrito_cantidad': len(Carrito(request))}
+
+
+def rol_usuario(request):
+    return {'es_vendedor': es_vendedor(request.user)}
--- tiendapurificadores/settings.py
+++ tiendapurificadores/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'pedidos.context_processors.carrito_cantidad',
+                'pedidos.context_processors.rol_usuario',
             ],
         },
     },
